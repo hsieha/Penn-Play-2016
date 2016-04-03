e67c300047b2f9fe3dce3786efe065e003ef3328
Annie Yang <annie.ghostie@gmail.com>
2016-04-03 10:25:59 -0400

    red alert

---
+++ AAdventure/Assets/Scripts/RedAlertScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedAlertScript : MonoBehaviour {
+    PlayerBehaviourScript playerScript;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviourScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviourScript>();
+        if (playerScript != null && playerScript.hasTreasure)
+        {
+            Debug.Log(" treasure");
+            animator.SetTrigger("TreasureFound");
+        }
+    }
+}
