4de9307c4f6d3c0687608a1e6d774334d2035ef2
hsieha <hsieha@seas.upenn.edu>
2016-04-03 01:35:59 -0400

    some basic random level generator

--- AAdventure/Assets/Scripts/LevelGeneratorScript.cs
+++ AAdventure/Assets/Scripts/LevelGeneratorScript.cs
@@ -8,15 +8,13 @@ public class LevelGeneratorScript : MonoBehaviour {
 	public Transform room;
 	public Transform player;
 
+	enum Entry{
+		Left, Right, Top, Bottom
+		};
+
 	// Use this for initialization
 	void Start () {
-		for (int i = -1; i < 2; i++) {
-			for (int j = -1; j < 2; j++) {
-				Transform r = (Transform) Instantiate (room, new Vector3 (i * 7, j * 7, 0), Quaternion.identity);
-				map.Add (new Tuple (i, j), r);
-			}
-		}
-
+		generateLevel ();
 		Instantiate (player, new Vector3 (0, 0, -1), Quaternion.AngleAxis(90, new Vector3(1,0,0)));
 	}
 	
@@ -24,4 +22,95 @@ public class LevelGeneratorScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void instantiateRoom(int x, int y, bool leftEntry, bool rightEntry, bool topEntry, bool bottomEntry) {
+		Transform r = (Transform)Instantiate (room, new Vector3 (x*7, y*7, 0), Quaternion.identity);
+		RoomScript rScript = r.GetComponentInChildren<RoomScript> ();
+		rScript.setWallEntries (leftEntry, rightEntry, topEntry, bottomEntry);
+		map.Add (new Tuple (x, y), r);
+
+	}
+
+	void generateLevel() {
+		Dictionary<Tuple, HashSet<int>> entryMap = new Dictionary<Tuple,HashSet<int>> ();
+		Tuple curTuple = new Tuple (0, 0);
+		entryMap.Add (curTuple, new HashSet<int> ());
+		while (entryMap.Count > 0) {
+			Debug.Log (curTuple.x + " " + curTuple.y);
+			foreach (Tuple t in entryMap.Keys) {
+				curTuple = t;
+				break;
+			}
+			HashSet<int> newEntries = randomizeRoom (curTuple.x, curTuple.y, entryMap[curTuple], 2);
+			foreach (int entry in newEntries) {
+				if (entry == (int)Entry.Left) {
+					Tuple left = new Tuple (curTuple.x - 1, curTuple.y);
+					if (entryMap.ContainsKey (left)) {
+						HashSet<int> oldEntries = entryMap [left];
+						oldEntries.Add ((int)Entry.Right);
+						entryMap.Add (left, oldEntries);
+					} else {
+						HashSet<int> oldEntries = new HashSet<int> ();
+						oldEntries.Add ((int)Entry.Right);
+						entryMap.Add (left, oldEntries);
+					}
+				} else if (entry == (int)Entry.Right) {
+					Tuple right = new Tuple (curTuple.x + 1, curTuple.y);
+					if (entryMap.ContainsKey (right)) {
+						HashSet<int> oldEntries = entryMap [right];
+						oldEntries.Add ((int)Entry.Left);
+						entryMap.Add (right, oldEntries);
+					} else {
+						HashSet<int> oldEntries = new HashSet<int> ();
+						oldEntries.Add ((int)Entry.Left);
+						entryMap.Add (right, oldEntries);
+					}
+				} else if (entry == (int)Entry.Top) {
+					Tuple top = new Tuple (curTuple.x, curTuple.y + 1);
+					if (entryMap.ContainsKey (top)) {
+						HashSet<int> oldEntries = entryMap [top];
+						oldEntries.Add ((int)Entry.Bottom);
+						entryMap.Add (top, oldEntries);
+					} else {
+						HashSet<int> oldEntries = new HashSet<int> ();
+						oldEntries.Add ((int)Entry.Bottom);
+						entryMap.Add (top, oldEntries);
+					}
+				} else if (entry == (int)Entry.Bottom) {
+					Tuple bottom = new Tuple (curTuple.x, curTuple.y - 1);
+					if (entryMap.ContainsKey (bottom)) {
+						HashSet<int> oldEntries = entryMap [bottom];
+						oldEntries.Add ((int)Entry.Top);
+						entryMap.Add (bottom, oldEntries);
+					} else {
+						HashSet<int> oldEntries = new HashSet<int> ();
+						oldEntries.Add ((int)Entry.Top);
+						entryMap.Add (bottom, oldEntries);
+					}
+				}
+			}
+			entryMap.Remove (curTuple);
+		}
+	}
+
+		HashSet<int> randomizeRoom(int x, int y, HashSet<int> entry, int layer) {
+		int count = entry.Count;
+		HashSet<int> entriesSet = new HashSet<int>();
+		foreach (int e in entry) {
+			entriesSet.Add (e);
+		}
+		HashSet<int> newEntries = new HashSet<int> ();
+		while (count < 3 - layer) {
+			int rand = Random.Range (0, 3);
+			while (entriesSet.Contains (rand)) {
+				rand = Random.Range (0, 3);
+			}
+			entriesSet.Add (rand);
+			newEntries.Add (rand);
+			count++;
+		}
+		instantiateRoom (x, y, !entriesSet.Contains (0), !entriesSet.Contains (1), !entriesSet.Contains (2), !entriesSet.Contains (3));
+		return newEntries;
+	}
+		
 }
--- AAdventure/Assets/Scripts/RoomScript.cs
+++ AAdventure/Assets/Scripts/RoomScript.cs
@@ -10,13 +10,29 @@ public class RoomScript : MonoBehaviour {
 	public Renderer rendereŕ;
 	public HashSet<Transform> walls;
 	public RoomInfoScript roomInfoScript;
+	public Transform leftWallEntry;
+	public Transform rightWallEntry;
+	public Transform topWallEntry;
+	public Transform bottomWallEntry;
 
 	// Use this for initialization
 	void Start () {
 		rendereŕ = GetComponent<Renderer> ();
 		walls = new HashSet<Transform> ();
 		foreach(Transform children in transform) {
+			
 			foreach (Transform wallpiece in children) {
+				if (wallpiece.name == "WallEntry") {
+					if (children.name == "LeftWall") {
+						leftWallEntry = wallpiece;
+					} else if (children.name == "RightWall") {
+						rightWallEntry = wallpiece;
+					} else if (children.name == "TopWall") {
+						topWallEntry = wallpiece;
+					} else if (children.name == "BottomWall") {
+						bottomWallEntry = wallpiece;
+					}
+				}
 				if (wallpiece.tag == "WallPiece") {
 					walls.Add (wallpiece);
 				}
@@ -29,6 +45,13 @@ public class RoomScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void setWallEntries(bool leftEntry, bool rightEntry, bool topEntry, bool bottomEntry) {
+		leftWallEntry.gameObject.SetActive (leftEntry);
+		rightWallEntry.gameObject.SetActive (rightEntry);
+		topWallEntry.gameObject.SetActive (topEntry);
+		bottomWallEntry.gameObject.SetActive (bottomEntry);
 	}
 
 	public void deactivate() {
