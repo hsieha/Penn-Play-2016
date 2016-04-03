f83cf099461b34bb62a0223d478d2d319ee76723
hsieha <hsieha@seas.upenn.edu>
2016-04-03 11:51:56 -0400

    added level text

---
+++ AAdventure/Assets/Scripts/LevelTextScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelTextScript : MonoBehaviour {
+	Text levelText;
+
+	// Use this for initialization
+	void Start () {
+		levelText = gameObject.GetComponent<Text>();
+		levelText.text = "LEVEL: " + PlayerPrefs.GetInt ("LevelNumber", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
