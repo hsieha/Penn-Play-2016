eefec7109d1eb1e30761fae1ffd6bb484a151ae7
hsieha <hsieha@seas.upenn.edu>
2016-04-03 01:53:44 -0400

    better level generator

--- AAdventure/Assets/Scripts/LevelGeneratorScript.cs
+++ AAdventure/Assets/Scripts/LevelGeneratorScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class LevelGeneratorScript : MonoBehaviour {
-	public Dictionary<Tuple, Transform> map = new Dictionary<Tuple, Transform> ();
+	public Dictionary<Tuple, Transform> map;
 
 	public Transform room;
 	public Transform player;
@@ -14,6 +14,7 @@ public class LevelGeneratorScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		map = new Dictionary<Tuple, Transform> ();
 		generateLevel ();
 		Instantiate (player, new Vector3 (0, 0, -1), Quaternion.AngleAxis(90, new Vector3(1,0,0)));
 	}
@@ -36,12 +37,11 @@ public class LevelGeneratorScript : MonoBehaviour {
 		Tuple curTuple = new Tuple (0, 0);
 		entryMap.Add (curTuple, new HashSet<int> ());
 		while (entryMap.Count > 0) {
-			Debug.Log (curTuple.x + " " + curTuple.y);
 			foreach (Tuple t in entryMap.Keys) {
 				curTuple = t;
 				break;
 			}
-			HashSet<int> newEntries = randomizeRoom (curTuple.x, curTuple.y, entryMap[curTuple], 2);
+			HashSet<int> newEntries = randomizeRoom (curTuple.x, curTuple.y, entryMap[curTuple], Mathf.Abs(curTuple.x)+Mathf.Abs(curTuple.y));
 			foreach (int entry in newEntries) {
 				if (entry == (int)Entry.Left) {
 					Tuple left = new Tuple (curTuple.x - 1, curTuple.y);
@@ -94,20 +94,18 @@ public class LevelGeneratorScript : MonoBehaviour {
 	}
 
 		HashSet<int> randomizeRoom(int x, int y, HashSet<int> entry, int layer) {
-		int count = entry.Count;
 		HashSet<int> entriesSet = new HashSet<int>();
 		foreach (int e in entry) {
 			entriesSet.Add (e);
 		}
 		HashSet<int> newEntries = new HashSet<int> ();
-		while (count < 3 - layer) {
+		while (entriesSet.Count < 3 - layer) {
 			int rand = Random.Range (0, 3);
 			while (entriesSet.Contains (rand)) {
 				rand = Random.Range (0, 3);
 			}
 			entriesSet.Add (rand);
 			newEntries.Add (rand);
-			count++;
 		}
 		instantiateRoom (x, y, !entriesSet.Contains (0), !entriesSet.Contains (1), !entriesSet.Contains (2), !entriesSet.Contains (3));
 		return newEntries;
